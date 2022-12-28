4a2e778436f5bd89eae6c2e270e50b3138ca17c3
Maksat <makezovs@gmail.ru>
2022-12-28 04:08:23 +0300

    Rollup Tz 2

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import styles from 'rollup-plugin-styles'
+import image from 'rollup-plugin-img'
+import { babel } from '@rollup/plugin-babel'
+import serve from 'rollup-plugin-serve'
+
 export default {
 	input: './index.js',
 	output: {
@@ -7,4 +12,16 @@ export default {
 	watch: {
 		exclude: 'node_modules/**',
 	},
+	plugins: [
+		styles(),
+		image({
+			limit: 100000,
+		}),
+		babel({ babelHelpers: 'bundled' }),
+		serve({
+			open: true,
+			contentBase: './',
+			port: 8080,
+		}),
+	],
 }
